f3f7d55a56a6ca695294f4d0d99f64e8102269cc
Cruz Prieto Jose Luis <141293124+Joseluiscruz-hub@users.noreply.github.com>
2025-11-06 21:52:49 -0600

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Check-List-de-Verticalidad/',
+      base: '/Check-List-Estivado-y-verticalidad/',
       server: {
         port: 3000,
         host: '0.0.0.0',
